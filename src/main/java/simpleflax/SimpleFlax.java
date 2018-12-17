ac81b9433b93a4bf1add64defe6007ddbc64b108
coderbot16 <coderbot16@gmail.com>
2018-12-16 16:59:31 -0800

    New mod file

---
+++ src/main/java/simpleflax/SimpleFlax.java
@@ -0,0 +1,31 @@
+package simpleflax;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = SimpleFlax.MODID, name = SimpleFlax.NAME, version = SimpleFlax.VERSION)
+public class SimpleFlax
+{
+	public static final String MODID = "simpleflax";
+	public static final String NAME = "Simple Flax";
+	public static final String VERSION = "0.1.0";
+
+	private static Logger logger;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		logger = event.getModLog();
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		// some example code
+		logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	}
+}
