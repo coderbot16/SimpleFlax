4e610935b0b6a7c6087d4e5205d8cd6c32e68e0a
coderbot <coderbot16@gmail.com>
2019-06-09 09:21:59 -0700

    Bump version to 1.2.1: French translation

--- src/main/java/simpleflax/SimpleFlax.java
+++ src/main/java/simpleflax/SimpleFlax.java
@@ -27,7 +27,7 @@ public class SimpleFlax
 {
 	public static final String MODID = "simpleflax";
 	public static final String NAME = "Simple Flax";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 
 	@SidedProxy(clientSide = "simpleflax.proxy.ClientProxy", serverSide = "simpleflax.proxy.CommonProxy")
 	public static CommonProxy proxy;
